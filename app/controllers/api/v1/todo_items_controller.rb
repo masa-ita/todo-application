e63f050b6e5be055d5eadd34bd650423f7329b71
Masatoshi Itagaki <masatoshi@rails.to>
2021-04-08 13:05:42 +0900

    complete CRUD actions on API

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -8,10 +8,41 @@ class Api::V1::TodoItemsController < ApplicationController
     def show
     end
     def create
+        @todo_item = current_user.todo_items.build(todo_item_params)
+        if authorized?
+          respond_to do |format|
+            if @todo_item.save
+              format.json { render :show, status: :created, location: api_v1_todo_item_path(@todo_item) }
+            else
+              format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+            end
+          end
+        else
+          handle_unauthorized
+        end
     end
     def update
+        if authorized?
+            respond_to do |format|
+              if @todo_item.update(todo_item_params)
+                format.json { render :show, status: :ok, location: api_v1_todo_item_path(@todo_item) }
+              else
+                format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+              end
+            end
+        else
+            handle_unauthorized
+        end
     end
     def destroy
+        if authorized?
+            @todo_item.destroy
+            respond_to do |format|
+              format.json { head :no_content }
+            end
+        else
+            handle_unauthorized
+        end
     end
     private
         def set_todo_item
@@ -28,5 +59,9 @@ class Api::V1::TodoItemsController < ApplicationController
                 format.json { render :unauthorized, status: 401 }
               end
             end
+        end
+
+        def todo_item_params
+            params.require(:todo_item).permit(:title, :complete)
         end
 end
